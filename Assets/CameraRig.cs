b8d81338573be9e107120d18b3d787c7224de970
Egor Slesarenko <egor050503sl@gmail.com>
2024-06-17 22:28:39 +0300

    add 11

--- Assets/CameraRig.cs
+++ Assets/CameraRig.cs
@@ -2,19 +2,22 @@ using UnityEngine;
 
 public class CameraRig : MonoBehaviour
 {
-    [SerializeField] private Transform character;
-    [SerializeField] private float smoothTime = 0.1f; //примерно
 
-    private Vector3 vel;
 
-    public void Update()
-    {
-        transform.position = Vector3.SmoothDamp(transform.position, character.position - Vector3.one, ref vel, smoothTime); //плавно перемещает камеру в точку координату персонажа
-        transform.forward = Vector3.SmoothDamp(transform.forward, character.forward, ref vel, smoothTime); //плавно перемещает forward (поворачивает) cameraRig чтобы смотреть в то же место, куда и персонаж.
 
-        //можно еще иметь ссылку на саму камеру и сделать что-то типа
-        transform.LookAt(character.position); // смотрит на персонажа
 
-        // ну также можно сюда засунуть облет препятствий, например, если сзади стена, то подлетаешь камерой (делаешь Z меньше) к персонажу
-    }
+    // Egor version
+    //[SerializeField] private Transform _character;
+    
+    //private Vector3 _offset;
+
+    //private void Start()
+    //{
+    //    _offset = _character.position - transform.position;
+    //}
+
+    //public void Update()
+    //{
+    //   transform.position = _character.position - _offset;
+    //}
 }
