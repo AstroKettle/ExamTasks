d742a776ab98483126dfb735d87a617dc9c3e507
AstroKettle <AstroFizik51@yandex.ru>
2024-06-17 22:35:53 +0300

    camera rig

--- Assets/CameraRig.cs
+++ Assets/CameraRig.cs
@@ -5,9 +5,11 @@ public class CameraRig : MonoBehaviour
 
 
 
+    // Second version
+    [SerializeField] private Transform _character;
+    [SerializeField] private float smoothTime = 0.05f; 
 
-    // Egor version
-    //[SerializeField] private Transform _character;
+    private Vector3 vel;
     
     //private Vector3 _offset;
 
@@ -20,4 +22,11 @@ public class CameraRig : MonoBehaviour
     //{
     //   transform.position = _character.position - _offset;
     //}
+
+    public void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, _character.position, ref vel, smoothTime); //плавно перемещает камеру в точку координату персонажа
+        transform.forward = Vector3.SmoothDamp(transform.forward, _character.forward, ref vel, smoothTime); //плавно перемещает forward (поворачивает) cameraRig чтобы смотреть в то же место, куда и персонаж.
+        
+    }
 }
